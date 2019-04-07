62d55bc23b7a1863b539e46014f48aa189021308
gabrielhv <11673590+gabrielhv@users.noreply.github.com>
2019-04-07 16:14:33 -0400

    GHV: vrai ajout de material

--- tp5/INF3710_TP5-ui/client/src/app/app.module.ts
+++ tp5/INF3710_TP5-ui/client/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from "@angular/common";
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -20,7 +21,8 @@ import { RoomComponent } from "./room/room.component";
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule
   ],
   providers: [CommunicationService],
   bootstrap: [AppComponent],
